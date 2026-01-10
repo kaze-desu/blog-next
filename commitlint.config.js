246ee60fc5ec4fd9a44e8ddd71c0a51898503c6d
Anda Toshiki <hello@toshiki.dev>
2026-01-09 22:06:35 -0700

    chore: update maximum character limits for commit messages in config

--- commitlint.config.js
+++ commitlint.config.js
@@ -5,7 +5,7 @@ export default {
     'type-case': [2, 'always', 'lower-case'],
     'scope-case': [2, 'always', 'lower-case'],
     'subject-case': [2, 'always', 'lower-case'],
-    
+
     // Standard conventional commit types
     'type-enum': [
       2,
@@ -24,10 +24,10 @@ export default {
         'test',
       ],
     ],
-    
+
     // Subject requirements
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
-    'header-max-length': [2, 'always', 100],
+    'header-max-length': [2, 'always', 200],
   },
 }
